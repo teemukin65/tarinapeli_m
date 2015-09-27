44f3738c3efa8a0142bb6ad7a0e28b9249a16905
Teemu Kurki <teemukin65@gmail.com>
2015-09-27 21:23:16 +0300

    Finnish locale added for angular, text boxes are now tighter spaced, eka demoilua varten

--- config/env/all.js
+++ config/env/all.js
@@ -18,6 +18,7 @@ module.exports = {
 			],
 			js: [
 				'public/lib/angular/angular.js',
+				'public/lib/angular/angular-locale_fi-fi.js',
 				'public/lib/angular-resource/angular-resource.js', 
 				'public/lib/angular-cookies/angular-cookies.js', 
 				'public/lib/angular-touch/angular-touch.js', 
